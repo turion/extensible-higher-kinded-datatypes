207c46a286f0c77474f17a6a298edd53cf0d7883
Manuel Bärenz <mbaerenz@heilmannsoftware.de>
2025-08-27 11:45:37 +0200

    Hide kind signature behind CPP

--- extensible-higher-kinded-datatypes/src/Data/HKD/Extensible.hs
+++ extensible-higher-kinded-datatypes/src/Data/HKD/Extensible.hs
@@ -22,12 +22,17 @@ class FunctorE (d :: Phases (aspect ': aspects) -> Type) where
 headMap :: (FunctorE d) => (forall x. Interpret aspect p1 x -> Interpret aspect p2 x) -> d (p1 '::: ps) -> d (p2 '::: ps)
 headMap f = phaseMap $ HeadMap f
 
+-- StandaloneKindSignatures is only available from 8.10 onwards. Remove once we drop support for lower GHCs
+#if MIN_VERSION_GLASGOW_HASKELL(8,10,0,0)
 type PhaseMap :: forall (aspects :: [Type]) -> Phases aspects -> Phases aspects -> Type
+#endif
 data PhaseMap aspects (phasesIn :: Phases aspects) (phasesOut :: Phases aspects) where
   HeadMap :: (forall x. Interpret aspect pIn x -> Interpret aspect pOut x) -> PhaseMap (aspect ': aspects) (pIn '::: ps) (pOut '::: ps)
   TailMap :: PhaseMap aspects pIn pOut -> PhaseMap (aspect ': aspects) (p '::: pIn) (p '::: pOut)
 
+#if MIN_VERSION_GLASGOW_HASKELL(8,10,0,0)
 type PhaseTraversal :: forall (aspects :: [Type]) -> Phases aspects -> Phases aspects -> (Type -> Type) -> Type
+#endif
 data PhaseTraversal aspects (phasesIn :: Phases aspects) (phasesOut :: Phases aspects) f where
   HeadTraversal :: (forall x. Interpret aspect pIn x -> f (Interpret aspect pOut x)) -> PhaseTraversal (aspect ': aspects) (pIn '::: ps) (pOut '::: ps) f
   TailTraversal :: PhaseTraversal aspects pIn pOut f -> PhaseTraversal (aspect ': aspects) (p '::: pIn) (p '::: pOut) f
